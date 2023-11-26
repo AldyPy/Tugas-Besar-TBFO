e39cc124b508dbb925faea8155a57ba20a5212c1
Rvans <shafiq.irvansyah@gmail.com>
2023-11-26 22:59:12 +0700

    add: finishingtouches

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from Input_Handling.PDAconfig import*
 from Input_Handling.htmltokenizer import*
 from PDA_Model.PushDownAutomaton import*
 import argparse
+from finishingtouches import*
 
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('pdaconfig', help='')
@@ -32,7 +33,8 @@ ErrorIndex = len(InputTokens) - RemainingTokensLength
 
 # Output
 if (Accepted):
-    print("\x1b[32mAccepted\033[0m")
+    printWibuAccepted()
+    print("Hatsune Miku says: \x1b[32m\"Yayy! Your HTML file got accepted-desu!\"\033[0m\n")
 else:
     if ErrorIndex == len(InputTokens) - 1: 
         # I don't know why but I guess the indexing got messed up somewhere
@@ -42,7 +44,8 @@ else:
         Line = rowinfo[ErrorIndex+1]
         Column = colinfo[ErrorIndex+1]
 
-    print(f"\x1b[31mSyntax Error\x1b[36m" + f" at [Ln {Line}, Col {Column}]:\033[0m\n", end="")
+    printWibuRejected()
+    print(f"Hatsune Miku says \"\x1b[31mSyntax Error\x1b[36m" + f" at [Ln {Line}, Col {Column}]: \"\033[0m\n", end="")
 
     # print the error line
     lines = read_file_lines(arguments.htmlpath)
